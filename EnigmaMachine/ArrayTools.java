7aaaa7c8caa5c26b1b3e3b89fee036a4a5781883
aiden-so <aiden.sob88@gmail.com>
2015-04-13 14:26:33 +0000

    Documentation and Optimization
    This pull request fully documents the ArrayTools class, as well as optimizes several of the algorithms/functions within it

--- EnigmaMachine/ArrayTools.java
+++ EnigmaMachine/ArrayTools.java
@@ -3,54 +3,48 @@
 
 public class ArrayTools
 {
+    //Draws the constant alphabetSize used for many functions from EnigmaMachine
     public static final int alphabetSize = EnigmaMachine.alphabetSize;
     
     private static GetRandom RNG = new GetRandom();
     
+    //Returns a standard-order alphabet
     public static char[] resetAlphabet()
     {
         char[] freshAlphabet = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         return freshAlphabet;
     }
     
+    //Returns a scrambled alphabet
     public static char[] randomAlphabetArray()
     {
-        char[] freshArray = new char[alphabetSize];
-        char[] alphabet = new char[alphabetSize];
-        alphabet = resetAlphabet();
+        char[] scrambledArray = new char[alphabetSize];
+        char[] freshAlphabet = new char[alphabetSize];
+        freshAlphabet = resetAlphabet();
         
         int currentIndex;
         
         for (int count = 0; count < alphabetSize; count++)
         {
-            if (alphabet.length > 1)
-            {
-                currentIndex = RNG.getRandom(alphabet.length - 1);
-            }
-            else if (alphabet.length == 1)
-            {
-                currentIndex = 0;
-            }
-            else
-            {
-                break;
-            }
-            
-            freshArray[count] = alphabet[currentIndex];
-            //Delete used array index
-            alphabet = removeChar(alphabet, currentIndex);
+            //Select a random index
+            currentIndex = RNG.getRandom(freshAlphabet.length - 1);
+            //Copy that char to freshArray
+            scrambledArray[count] = freshAlphabet[currentIndex];
+            //Delete used array index from the standard alphabet
+            freshAlphabet = removeChar(freshAlphabet, currentIndex);
         }
         
-        alphabet = resetAlphabet();
-        
-        return freshArray;
+        return scrambledArray;
     }
     
+    //Removes an index from a given array at a given index
     private static char[] removeChar(char[] inputArray, int index)
     {
+        //New array with length of one less than input
         char[] modifiedArray = new char[inputArray.length - 1];
         int currentIndex = 0;
         
+        //Loops through the input array, copies over each index if it ISNT eqaul to the given index of inputArray
         for (char current : inputArray)
         {
             if (current != inputArray[index])
@@ -63,12 +57,14 @@ public class ArrayTools
         return modifiedArray;
     }
     
+    //Returns the given rotor-array, shifted one position down
     public static char[] shift(char[] rotorArray)
     {
         char[] outputRotor = new char[alphabetSize];
-        //move the carry-over char
+        //Move the char of the last position to the first
         outputRotor[0] = rotorArray[rotorArray.length - 1];
         
+        //Loops through the array and makes the previous index value the current one
         for (int index = rotorArray.length - 1; index > 0; index--)
         {
             outputRotor[index] = rotorArray[index - 1];
@@ -77,30 +73,30 @@ public class ArrayTools
         return outputRotor;
     }
     
+    //Returns the index value of the given char in a given array
     public static int getIndex(char input, char[] array)
     {
-        int index = 0;
-        
+        //Returns c (the index) as soon as it is found
         for (int c = 0; c < alphabetSize; c++)
         {
             if (array[c] == input)
             {
-                index = c;
+                return c;
             }
         }
        
-        return index;
+        //Returns -1 if the index can not be found
+        return -1;
     }
     
+    //Prints a given array with a space between each index
     public static void printArray(char[] array)
     {
-        StringBuilder output = new StringBuilder();
-        
         for (char current : array)
         {
-            output.append(current + " ");
+            System.out.print(current + " ");
         }
         
-        System.out.println(output.toString());
+        System.out.println();
     }
 }
