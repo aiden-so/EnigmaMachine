3a47af2e2e65b6014eacf29ea55767299f991456
aiden-so <aiden.sob88@gmail.com>
2015-04-16 11:17:38 +0000

    Cut out unnecessary parts
    Optimized and slimmed down

--- EnigmaMachine/Keyboard.java
+++ EnigmaMachine/Keyboard.java
@@ -3,46 +3,28 @@ import java.lang.*;
 
 //This class acts as the input-capture for the enigma-machine- it captures input then converts it
 //into a suitable format (char[], lower case)
-//This class has not yet been fully integrated with EnigmaMachine, a lot of it is unused
 public class Keyboard
 {
-    public Keyboard()
-    {
-        //Empty at this point in time
-    }
-    
     //Gets a string input, converts it to a char array, makes it lower case then returns it
     public char[] getFormattedInput(String input)
     {
-        char[] output = new char[input.length()];
-        
-        output = convertCharArrayToLower(parseToChar(input));
-        
-        return output;
+        return convertCharArrayToLower(parseToChar(input));
     }
     
-    //Uses inbuilt methods in Java to parse a String to a char array, which it then returns
+    //parse a String to a char array, then return
     public char[] parseToChar(String input)
     {
-        char[] charOutput = new char[input.length()];
-        
-        charOutput = input.toCharArray();
-        
-        return charOutput;
+        return input.toCharArray();
     }
     
-    //Initializes a new char array, then for every index of the input array it converts it to lower case then stores
-    //it in the output array
+    //for every index of the input array it converts it to lower case then returns
     public char[] convertCharArrayToLower(char[] input)
     {
-        char[] output = new char[input.length];
-        
         for (int index = 0; index < input.length; index++)
         {
-            //Calls the character object method imported from java.lang.*
-            output[index] = Character.toLowerCase(input[index]);
+            input[index] = Character.toLowerCase(input[index]);
         }
         
-        return output;
+        return input;
     }
 }
